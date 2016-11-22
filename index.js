2011b0cf66048aa3b3634c52416dbbc331878d28
Kevin Goslar <kevin.goslar@gmail.com>
2016-11-22 08:25:27 -0800

    EsLint

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ var psTree = require('ps-tree')
 module.exports = function(done) {
   psTree(process.pid, function(err, children) {
     if (err) return done(err)
-    for (i = 0; i < children.length; i++) {
+    for (var i = 0; i < children.length; i++) {
       try {
         process.kill(children[i].PID)
       } catch (e) {
-        console.log(e)
         return done(e)
       }
     }
