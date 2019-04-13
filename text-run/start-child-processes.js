f2440c697f47091c76cd98fb7775b946ce4b9212
Kevin Goslar <kevin.goslar@originate.com>
2019-04-12 21:29:26 -0500

    Update dependencies (#2)
    * Update dependencies
    
    * Revert

--- text-run/start-child-processes.js
+++ text-run/start-child-processes.js
@@ -3,12 +3,12 @@ const N = require('nitroglycerin')
 const psTree = require('ps-tree')
 
 module.exports = function ({ formatter }, done) {
-  formatter.output('starting child processes')
+  formatter.log('starting child processes')
   childProcess.exec('(read foo)')
   psTree(
     process.pid,
     N(children => {
-      formatter.output(`${children.length - 1} child processes are running now`)
+      formatter.log(`${children.length - 1} child processes are running now`)
       done()
     })
   )
