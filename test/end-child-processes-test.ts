66618548a8cb7d6f1be1ed32b525ac640ee67644
Kevin Goslar <kevin.goslar@gmail.com>
2019-04-13 03:21:51 +0000

    Typescript

---
+++ test/end-child-processes-test.ts
@@ -0,0 +1,36 @@
+import { expect } from 'chai'
+import childProcess from 'child_process'
+import { describe, it } from 'mocha'
+import psTree from 'ps-tree'
+import { endChildProcesses } from '../src/index'
+
+describe('end-child-processes', () => {
+  it('ends all child processes', done => {
+    // start some child processes
+    childProcess.exec('(read foo)')
+    runningProcessCount(function(err: Error, count: number) {
+      if (err) {
+        return done(err)
+      }
+      expect(count).to.equal(2)
+
+      // stop the child processes
+      endChildProcesses(function() {
+        runningProcessCount(function(err2: Error, count2: number) {
+          if (err2) {
+            return done(err2)
+          }
+          // console.log(`running ${count} subprocesses`)
+          expect(count2).to.equal(0)
+          done()
+        })
+      })
+    })
+  })
+})
+
+function runningProcessCount(done) {
+  psTree(process.pid, (err, children) => {
+    done(err, children.length - 1)
+  })
+}
