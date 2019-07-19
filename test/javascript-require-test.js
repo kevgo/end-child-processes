d0ce29edc8c763b2a96304123431a8d9a047e39b
Kevin Goslar <kevin.goslar@gmail.com>
2019-07-18 22:10:43 -0500

    Remove obsolete tests

--- test/javascript-require-test.js
+++
@@ -1,8 +0,0 @@
-const { expect } = require('chai')
-
-describe('loading via JavaScript', () => {
-  it('can be required via require', () => {
-    const { endChildProcesses } = require('..')
-    expect(endChildProcesses).to.not.be.null
-  })
-})
