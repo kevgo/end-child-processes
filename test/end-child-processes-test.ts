39de45a8ac3740fb282601382ba012cdec0cd830
Kevin Goslar <kevin.goslar@gmail.com>
2020-02-21 07:40:47 -0600

    Simplify test structure

--- test/end-child-processes-test.ts
+++ test/end-child-processes-test.ts
@@ -1,23 +1,21 @@
 import { strict as assert } from "assert"
 import childProcess from "child_process"
-import { describe, it } from "mocha"
+import { test } from "mocha"
 import psTree from "ps-tree"
 import util from "util"
 import { endChildProcesses } from "../src/index"
 const psTreeA = util.promisify(psTree)
 
-describe("end-child-processes", () => {
-  it("ends all child processes", async () => {
-    // start some child processes
-    childProcess.exec(blockingCommand())
-    let count = await runningProcessCount()
-    assert.equal(count, 2, "should have 2 child processes")
+test("end-child-processes", async function() {
+  // start some child processes
+  childProcess.exec(blockingCommand())
+  let count = await runningProcessCount()
+  assert.equal(count, 2, "should have 2 child processes")
 
-    // stop the child processes
-    await endChildProcesses()
-    count = await runningProcessCount()
-    assert.equal(count, 0, "should have 0 child processes now")
-  })
+  // stop the child processes
+  await endChildProcesses()
+  count = await runningProcessCount()
+  assert.equal(count, 0, "should have 0 child processes now")
 })
 
 /**
