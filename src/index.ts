55d69719f6c6547ab5bf30eafebfd49a9285b4f9
Kevin Goslar <kevin.goslar@gmail.com>
2020-10-28 12:31:30 -0500

    committing all files

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,22 @@
-import deb from "debug";
-import psTree from "ps-tree";
-import util from "util";
-const debug = deb("end-child-processes");
-const psTreeA = util.promisify(psTree);
+import deb from "debug"
+import psTree from "ps-tree"
+import util from "util"
+const debug = deb("end-child-processes")
+const psTreeA = util.promisify(psTree)
 
 export async function endChildProcesses(): Promise<void> {
-  const children = await psTreeA(process.pid);
+  const children = await psTreeA(process.pid)
   for (const child of children) {
     if (child.COMMAND === "ps") {
-      continue;
+      continue
     }
-    const processID = parseInt(child.PID, 10);
-    debug(`ending child process ${processID}: ${child.COMMAND}`);
+    const processID = parseInt(child.PID, 10)
+    debug(`ending child process ${processID}: ${child.COMMAND}`)
     try {
-      process.kill(processID);
+      process.kill(processID)
     } catch (e) {
-      debug(
-        `cannot kill process ${processID} (${child.COMMAND}): ${e.message}`
-      );
+      // eslint-disable-next-line
+      debug(`cannot kill process ${processID} (${child.COMMAND}): ${e.message}`)
     }
   }
 }
