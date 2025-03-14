361a44f0152fc649ed0dadfb77022a85d2e9926d
Kevin Goslar <kevin.goslar@gmail.com>
2025-03-14 09:25:54 -0500

    Remove debug dependency

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 import { psTree } from "@fengmk2/ps-tree"
-import deb from "debug"
 import * as util from "util"
-const debug = deb("end-child-processes")
 const psTreeA = util.promisify(psTree)
 const delay = util.promisify(setTimeout)
 
@@ -15,13 +13,10 @@ export async function endChildProcesses(): Promise<void> {
       continue
     }
     const processID = parseInt(child.PID, 10)
-    debug(`ending child process ${processID}: ${child.COMMAND}`)
     try {
       process.kill(processID)
     } catch (e) {
-      debug(
-        `cannot kill process ${processID} (${child.COMMAND}): ${(e as Error).message}`,
-      )
+      throw new Error(`cannot kill process ${processID} (${child.COMMAND}): ${(e as Error).message}`)
     }
   }
   await delay(1)
