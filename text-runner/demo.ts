3191cbbf6bac0376592a4d0c59b9cd4ffa3742e9
Kevin Goslar <kevin.goslar@gmail.com>
2025-03-20 06:10:02 -0500

    demo.ts

--- text-runner/demo.ts
+++ text-runner/demo.ts
@@ -1,44 +1,29 @@
 import { exec } from "child_process"
-import * as fs from "node:fs"
+import * as fs from "node:fs/promises"
 import * as tr from "text-runner"
 
-export async function demoScript(action: tr.actions.Args, done: (err: NodeJS.ErrnoException | null) => void) {
-  action.name("verify demo script")
-  const filePath = "../dist/test/demo.js"
+export async function demoScript(action: tr.actions.Args) {
+  action.name("verify demo script in README.md")
+
   // create the test file
+  await fs.mkdir("../dist/text-runner")
+  const filePath = "../dist/test/text-runner/demo.js"
   const fileContent = action.region.text()
-  fs.writeFile(filePath, fileContent, (err) => {
-    if (err) {
-      done(err)
+  await fs.writeFile(filePath, fileContent)
+
+  // execute the test file
+  exec(`npm exec node ${filePath}`, { cwd: "../src" }, (err, stdout, stderr) => {
+    const output = stdout + stderr
+    if (err || output !== "") {
+      if (output) {
+        console.log(output)
+      }
+      fs.rm(filePath, () => {
+        done(err ?? new Error("TypeScript run failed"))
+      })
       return
     }
-    // type-check the test file
-    exec(`npm exec tsc`, { cwd: ".." }, (err, stdout, stderr) => {
-      const output = stdout + stderr
-      if (err || output !== "") {
-        if (output) {
-          console.log(output)
-        }
-        fs.rm(filePath, () => {
-          done(err ?? new Error("TypeScript check failed"))
-        })
-        return
-      }
-      // execute the test file
-      exec(`npm exec node ${filePath}`, { cwd: "../src" }, (err, stdout, stderr) => {
-        const output = stdout + stderr
-        if (err || output !== "") {
-          if (output) {
-            console.log(output)
-          }
-          fs.rm(filePath, () => {
-            done(err ?? new Error("TypeScript run failed"))
-          })
-          return
-        }
-        // cleanup
-        fs.rm(filePath, done)
-      })
-    })
+    // cleanup
+    fs.rm(filePath, done)
   })
 }
