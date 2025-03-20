52d01086419d945cb1bd6669c6cf076c379508a0
Kevin Goslar <kevin.goslar@gmail.com>
2025-03-20 06:22:39 -0500

    demo.ts

--- text-runner/demo.ts
+++ text-runner/demo.ts
@@ -1,32 +1,25 @@
 import { exec } from "child_process"
 import * as fs from "node:fs/promises"
 import * as tr from "text-runner"
+import * as util from "util"
+const execa = util.promisify(exec)
 
 export async function demoScript(action: tr.actions.Args) {
   action.name("verify demo script in README.md")
 
   // create the test file
-  await fs.mkdir("../dist/text-runner")
-  const filePath = "../dist/test/text-runner/demo.js"
+  await fs.mkdir("../dist/text-runner", { recursive: true })
+  const filePath = "../dist/text-runner/demo.js"
   const fileContent = action.region.text()
   const replaced = replaceImport(fileContent)
   await fs.writeFile(filePath, replaced)
 
   // execute the test file
-  exec(`node ${filePath}`, { cwd: "../src" }, (err, stdout, stderr) => {
-    const output = stdout + stderr
-    if (err || output !== "") {
-      if (output) {
-        console.log(output)
-      }
-      fs.rm(filePath, () => {
-        done(err ?? new Error("TypeScript run failed"))
-      })
-      return
-    }
-    // cleanup
-    fs.rm(filePath, done)
-  })
+  const { stdout, stderr } = await execa("node demo.js", { cwd: "../dist/text-runner" })
+  const output = stdout + stderr
+  if (output !== "") {
+    throw new Error(`run produced output: ${output}`)
+  }
 }
 
 function replaceImport(text: string): string {
