9bece4a35139ef7ef4aa5a5644d7f621f8975436
Kevin Goslar <kevin.goslar@gmail.com>
2025-03-20 06:25:02 -0500

    demo.ts

--- text-runner/demo.ts
+++ text-runner/demo.ts
@@ -1,5 +1,6 @@
 import { exec } from "child_process"
 import * as fs from "node:fs/promises"
+import * as path from "path"
 import * as tr from "text-runner"
 import * as util from "util"
 const execa = util.promisify(exec)
@@ -8,14 +9,16 @@ export async function demoScript(action: tr.actions.Args) {
   action.name("verify demo script in README.md")
 
   // create the test file
-  await fs.mkdir("../dist/text-runner", { recursive: true })
-  const filePath = "../dist/text-runner/demo.js"
+  const dir = "../dist/text-runner"
+  const fileName = "demo.js"
+  await fs.mkdir(dir, { recursive: true })
+  const filePath = path.join(dir, fileName)
   const fileContent = action.region.text()
   const replaced = replaceImport(fileContent)
   await fs.writeFile(filePath, replaced)
 
   // execute the test file
-  const { stdout, stderr } = await execa("node demo.js", { cwd: "../dist/text-runner" })
+  const { stdout, stderr } = await execa(`node ${fileName}`, { cwd: dir })
   const output = stdout + stderr
   if (output !== "") {
     throw new Error(`run produced output: ${output}`)
